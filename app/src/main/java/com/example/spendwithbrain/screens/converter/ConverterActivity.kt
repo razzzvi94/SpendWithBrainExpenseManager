def0245bb495e853e814a5a8a82b44f1f3e1e7a5
Razvan <prundel.razvan@gmail.com>
2020-08-12 12:08:34 +0300

    Created activities for all screens

---
+++ app/src/main/java/com/example/spendwithbrain/screens/converter/ConverterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.spendwithbrain.screens.converter
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.spendwithbrain.R
+
+class ConverterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_converter)
+    }
+}
---
+++ app/src/main/java/com/example/spendwithbrain/screens/login/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.spendwithbrain.screens.login
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.spendwithbrain.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
