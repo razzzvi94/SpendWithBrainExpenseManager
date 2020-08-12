2fe6789bfbcfa7d642e82a5944d2af81a8cd72d3
Razvan <prundel.razvan@gmail.com>
2020-08-12 11:56:13 +0300

    Created blank Add Action Activity

---
+++ app/src/main/java/com/example/spendwithbrain/screens/addaction/AddActionActivity.kt
@@ -0,0 +1,12 @@
+package com.example.spendwithbrain.screens.addaction
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.spendwithbrain.R
+
+class AddActionActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_action)
+    }
+}
